4eb254c4881523c2e2d4532e9a5e3afe71596338
Alex <prokofe@gmail.com>
2018-08-14 16:59:18 +0300

    BloghCMS ver 0.9a
    1) Added class loader component
    2) For password hashing switched from md5 to bcrypt
    3) Moved app settings to config folder
    4) Minor fixes

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,5 +1,5 @@
 <div class="col-sm-12 footer">
-    &copy 2018 powered by <a href="https://github.com/Prokofe/Blogh">BloghCMS 0.8</a>
+    &copy 2018 powered by <a href="https://github.com/Prokofe/Blogh">BloghCMS 0.9</a>
 </div>
 </div>
 </body>
